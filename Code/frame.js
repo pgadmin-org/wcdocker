3d87dbd71fa78244ee80ffcd66de8d9d480974fd
Nagesh Dhope <nagesh.dhope@enterprisedb.com>
2020-01-01 06:31:53 -0500

    Added aria-label for Scroll left and Scroll right button in frame and tabframe

--- Code/frame.js
+++ Code/frame.js
@@ -372,8 +372,8 @@ define([
             this.$tabBar = $('<div class="wcFrameTitleBar">');
             this.$tabScroll = $('<div class="wcTabScroller">');
             this.$center = $('<div class="wcFrameCenter wcPanelBackground">');
-            this.$tabLeft = $('<div class="wcFrameButton" title="Scroll tabs to the left."><span class="fa fa-chevron-left"></span></div>');
-            this.$tabRight = $('<div class="wcFrameButton" title="Scroll tabs to the right."><span class="fa fa-chevron-right"></span></div>');
+            this.$tabLeft = $('<div class="wcFrameButton" title="Scroll tabs to the left." aria-label="Scroll left"><span class="fa fa-chevron-left"></span></div>');
+            this.$tabRight = $('<div class="wcFrameButton" title="Scroll tabs to the right." aria-label="Scroll right"><span class="fa fa-chevron-right"></span></div>');
             this.$close = $('<div class="wcFrameButton" title="Close the currently active panel tab" aria-label="Close panel"><div class="fa fa-close"></div></div>');
 
             this.$collapse = $('<div class="wcFrameButton" title="Collapse the active panel"><div class="fa fa-download"></div>C</div>');
